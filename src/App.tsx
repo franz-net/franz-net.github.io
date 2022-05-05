81ba2a6914d2da0713bac9bb17a642bcabca0825
Franz <5334409+franz-net@users.noreply.github.com>
2022-05-04 22:41:22 -0500

    base app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter, Routes} from "react-router-dom";
+
+function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <h1>
+                    Franz R.
+                </h1>
+                <Routes>
+
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
