11eba00b874135048edaadaf311efefb0a88cdb2
Franz <5334409+franz-net@users.noreply.github.com>
2022-05-05 23:42:10 -0500

    testing spline btn

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Button() {
+    return (
+        <div>
+            <Spline scene="https://prod.spline.design/y5JPzJkcO-v2wJPe/scene.splinecode"/>
+        </div>
+    );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ export default function Header() {
     return (
         <div className='header'>
             <h1 className='flex-1'>Franz R.</h1>
+            <Header/>
         </div>
     )
 }
