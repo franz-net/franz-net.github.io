b7c0e2f9e8c73f5de855d76f748c938608bc55d4
Franz <5334409+franz-net@users.noreply.github.com>
2022-05-17 22:00:18 -0500

    removed unused vars

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,9 +1,8 @@
-import {Switch, useTheme} from "@nextui-org/react";
+import {Switch} from "@nextui-org/react";
 import useDarkMode from '@fisch0920/use-dark-mode';
 
 export default function ThemeSwitch() {
     const darkMode = useDarkMode(false)
-    const {type, isDark} = useTheme()
     return (
         <>
             <Switch checked={darkMode.value} onChange={() => darkMode.toggle()}/>
