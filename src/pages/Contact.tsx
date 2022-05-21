58a399d72e9b77288aebc65977c5ae02aad7136a
Franz <5334409+franz-net@users.noreply.github.com>
2022-05-20 23:31:40 -0500

    adding some links

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,16 @@
-import {Text} from "@nextui-org/react";
+import {Button, Text} from "@nextui-org/react";
+import {FaGithubAlt} from "react-icons/fa";
 
 export default function Contact() {
     return (
         <div>
             <Text h2 css={{mx: 0, my: 3}} size={60}>Let's talk!</Text>
+            <Button
+                shadow
+                auto
+                color="error"
+                icon={<FaGithubAlt/>}
+            />
         </div>
     )
 }
